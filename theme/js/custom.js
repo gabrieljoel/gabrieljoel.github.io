163ab2f8bb7917725d1cecfb8a43762ef2e5f2d6
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2016-08-25 13:42:54 -0500

    Update documentation

---
+++ theme/js/custom.js
@@ -0,0 +1,25 @@
+(function($, window) {
+
+    $('#year').html(new Date().getFullYear());
+
+    $("a.scroll").on('click', function(e) {
+
+        // prevent default anchor click behavior
+        e.preventDefault();
+
+        // store hash
+        var hash = this.hash;
+
+        // animate
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 500, function(){
+
+            // when done, add hash to url
+            // (default click behaviour)
+            window.location.hash = hash;
+        });
+
+    });
+
+}).call(this, jQuery, window);
